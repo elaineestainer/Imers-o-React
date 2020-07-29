d1ee70b65e2d26b163f71f8e37d808681401efef
elaineestainer <elaineestainer@yahoo.com.br>
2020-07-29 12:23:14 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
+
+
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Switch>
+  < Route path="/" component={Home} exact/>
+  < Route path="/cadastro/video" component={CadastroVideo}/>
+  < Route path="/cadastro/Categoria" component={CadastroCategoria}/>
+  < Route component={Pagina404}/>
+  </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+
